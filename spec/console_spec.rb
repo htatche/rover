cda25d95cec20249bdb42ba2f2660147a8a2fd50
Hervé Tatche <herve.tatche@gmail.com>
2020-09-22 21:44:30 +0200

    add global spec for console interpreter

---
+++ spec/console_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../console.rb'
+
+RSpec.describe Console do
+  subject { described_class.new }
+
+  describe '#read' do
+    let(:input) { StringIO.new }
+
+    it 'reads the commands and outputs each rover final position and bearing' do
+      input.puts '5 5'
+      input.puts '1 2 N'
+      input.puts 'LMLMLMLMM'
+      input.puts '3 3 E'
+      input.puts 'MMRMMRMRRM'
+      input.rewind
+
+      $stdin = input
+
+      msg = <<~OUTPUT
+        1 3 N
+        5 1 E
+      OUTPUT
+
+      expect { subject.read }.to output(msg).to_stdout
+
+      $stdin = STDIN
+    end
+  end
+end
